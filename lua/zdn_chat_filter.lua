4739e09e3b63afa4d8da5e871a895ae2280f70dd
SMLDN <sakmyapeo@gmail.com>
2022-07-11 00:08:34 +0900

    coc tam ok

--- lua/zdn_chat_filter.lua
+++ lua/zdn_chat_filter.lua
@@ -106,6 +106,10 @@ function FilterCommand(chatStr)
     util_show_form("form_zdn_nhat_do", true)
     return true
   end
+  if nx_widestr(chatStr) == nx_widestr("/coc") then
+    util_show_form("form_zdn_coc", true)
+    return true
+  end
 
   -- for debug
   if nx_widestr(chatStr) == nx_widestr("/l") then
